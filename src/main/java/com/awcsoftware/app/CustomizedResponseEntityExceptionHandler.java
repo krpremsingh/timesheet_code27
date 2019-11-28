c41ddd4bc73da0dc9966da95c4d19798bc7a7014
Arun Kumar <akumar1@awcsoftware.net>
2019-11-28 10:42:01 +0530

    mybatis,exception handler added

--- src/main/java/com/awcsoftware/app/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/awcsoftware/app/CustomizedResponseEntityExceptionHandler.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	  @Override
 	  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 	      HttpHeaders headers, HttpStatus status, WebRequest request) {
-	    ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),"Validation Failed",
-	        ex.getBindingResult().toString());
+		  BindingResult errors = ex.getBindingResult();
+		   FieldError error = errors.getFieldError();
+		   logger.debug(error.getDefaultMessage());  
+	    ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),
+	    		ex.getBindingResult().toString(),ex.getBindingResult().getFieldError().getDefaultMessage());
 	    return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 	  }
 }
