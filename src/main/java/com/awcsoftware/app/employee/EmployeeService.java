3afa8c5cf85d97b19d7c466c2727e78f5865dd30
arun31018 <arunkumar31018@gmail.com>
2020-01-10 13:10:10 +0530

    Employee crud code added

--- src/main/java/com/awcsoftware/app/employee/EmployeeService.java
+++ src/main/java/com/awcsoftware/app/employee/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.awcsoftware.app.employee;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.mail.MessagingException;
@@ -228,6 +229,16 @@ public class EmployeeService {
 		}		
 		return EmployeeMessageConstants.EmployeeUpdated.getLabel().toString();
 
+	}
+	
+	public User getEmployee(int empId)throws AppException, DbException {
+		return empDao.getEmployee(empId);
+		
+	}
+	
+	public List<User> getEmployees() throws AppException,DbException{
+		return empDao.getEmployees();
+		
 	}
 
 }
